27ca50370e475a8e75ce169bba0d9a7dfd3c7650
wandumi <munandih@gmail.com>
2025-01-10 03:58:18 +0200

    Answer to question 1

---
+++ app/Http/Controllers/Json/JsonController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Json;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class JsonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $getFile = storage_path('app/json_data.json'); 
+        $jsonData = [];
+
+        if (file_exists($getFile)) { 
+            $jsonData = json_decode(file_get_contents($getFile), true); 
+        }
+
+        usort($jsonData, function ($a, $b) { 
+            return Carbon::parse($b['current_date'])->timestamp - Carbon::parse($a['current_date'])->timestamp; 
+        });
+
+        return view('JsonFiles.index', compact('jsonData'));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([ 
+            'product_name' => 'required|string|max:255',
+            'quantity_stock' => 'required|integer', 
+            'item_price' => 'required|integer', 
+        ]);
+
+        $data['current_date'] = Carbon::now()->toDateTimeString();
+        $data['total_value'] = $data['quantity_stock'] * $data['item_price'];
+
+        $getFile = storage_path('app/json_data.json'); 
+
+        $fileExist = file_exists($getFile) ? json_decode(file_get_contents($getFile), true) : []; 
+
+        $fileExist[] = $data;
+
+        $jsonData = json_encode($fileExist, JSON_PRETTY_PRINT);
+
+        Storage::disk('local')->put('json_data.json', $jsonData);
+
+        return back()->with(['message' => 'Successfully saved to a JSON file!']);
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    
+}
